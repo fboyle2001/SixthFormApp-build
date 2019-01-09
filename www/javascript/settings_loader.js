df6687a42a7799b629d33a36eb7f6ef38379eb86
fboyle2001 <finlayboyle2001@gmail.com>
2019-01-09 22:59:43 +0000

    v0.6.2-beta-rc1

--- www/javascript/settings_loader.js
+++ www/javascript/settings_loader.js
@@ -1,12 +1,133 @@
-function produceStyleElement() {
-  var stylesheet = "light.css";
+// All of the themes
+// Opted to store them out of the settings object as they can then be changed
+// without needing to change user's settings. Instead they will simply be
+// correct and consistent.
+//
+// In images, the key needs to correspond with the elements ID so that this
+// loader will auto-load the correct image.
+var __themes = {
+  light: {
+    name: "light",
+    stylesheet: "light.css",
+    images: {
+      login_logo: "light_mca.png"
+    }
+  },
+  dark: {
+    name: "dark",
+    stylesheet: "dark.css",
+    images: {
+      login_logo: "dark_mca.png"
+    }
+  },
+  sidebar_light: {
+    name: "sidebar_light",
+    stylesheet: "sidebar_light.css",
+    images: {
+      login_logo: "light_mca.png"
+    }
+  }
+}
 
-  if(Cookies.get("stylesheet") !== undefined) {
-    stylesheet = Cookies.get("stylesheet");
+// Store them as they will be used by other functions
+// try to hide them though as global variables in this could cause major issues
+// as this file is always called
+var __settings = loadSettings();
+
+function getUserSettings() {
+  return __settings;
+}
+
+function getUserTheme() {
+  return __themes[getUserSettings().theme];
+}
+
+// Load the user's settings
+function loadSettings() {
+  var saved = Cookies.get("settings");
+
+  if(saved == undefined) {
+    // Default settings
+    // Must stringify otherwise will error on first try
+
+    saved = JSON.stringify({
+      theme: "light",
+      scalable: false
+    });
+
+    // Should last for their entire duration at sixth form (4 years to account
+    // for issues caused by leap days)
+    Cookies.set("settings", saved, {expires: 1460});
   }
 
-  var link = '<link href="./css/' + stylesheet + '" rel="stylesheet" type="text/css">';
+  return JSON.parse(saved);
+}
+
+function produceStyleElement() {
+  var theme = getUserTheme();
+
+  var link = '<link href="./css/' + theme.stylesheet + '" rel="stylesheet" type="text/css">';
   return link;
 }
 
+function produceViewportElement() {
+  var scalableValue = getUserSettings().scalable ? "yes" : "no";
+
+  var tag = '<meta name="viewport" content="user-scalable=' + scalableValue + ', initial-scale=1, maximum-scale=2, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi"/>';
+  return tag;
+}
+
+function loadElements() {
+  var theme = getUserTheme();
+  var images = theme.images;
+
+  for(var key in images) {
+    // Element exists on this page
+    if(document.getElementById(key) != null) {
+      $("#" + key).attr("src", "./images/" + images[key]);
+    }
+  }
+
+  if(theme.name == "sidebar_light") {
+    // Load the images
+
+    $("#nav_home").text("");
+
+    if($("#nav_home").hasClass("current")) {
+      $("#nav_home").append('<img width="32" height="32" src="./images/nav_home_clicked.png">');
+    } else {
+      $("#nav_home").append('<img width="32" height="32" src="./images/nav_home.png">');
+    }
+
+    $("#nav_documents").text("");
+
+    if($("#nav_documents").hasClass("current")) {
+      $("#nav_documents").append('<img width="32" height="32" src="./images/nav_documents_clicked.png">');
+    } else {
+      $("#nav_documents").append('<img width="32" height="32" src="./images/nav_documents.png">');
+    }
+
+    $("#nav_links").text("");
+
+    if($("#nav_links").hasClass("current")) {
+      $("#nav_links").append('<img width="32" height="32" src="./images/nav_links_clicked.png">');
+    } else {
+      $("#nav_links").append('<img width="32" height="32" src="./images/nav_links.png">');
+    }
+
+    $("#nav_settings").text("");
+
+    if($("#nav_settings").hasClass("current")) {
+      $("#nav_settings").append('<img width="32" height="32" src="./images/nav_settings_clicked.png">');
+    } else {
+      $("#nav_settings").append('<img width="32" height="32" src="./images/nav_settings.png">');
+    }
+  }
+}
+
 $("head").append(produceStyleElement());
+$("head").append(produceViewportElement());
+
+$(document).ready(function() {
+  loadElements();
+});
