4f6fe8562b478f3b3808a077f0596c05478808bf
fboyle2001 <finlayboyle2001@gmail.com>
2019-01-13 12:09:47 +0000

    Debug

--- www/javascript/debug.js
+++ www/javascript/debug.js
@@ -15,8 +15,14 @@ function loadPage() {
   } else {
     $("#b").text("Window is defined");
   }
+  
+  if(typeof screen === undefined) {
+    $("#f").text("Window is undefined");
+  } else {
+    $("#f").text("Window is defined");
+  }
 
-  var conn = window.screen || {exists: "none"};
+  var conn = navigator.connection || {exists: "none"};
   conn = JSON.stringify(conn);
 
   $("#c").text("Conn: " + conn);
@@ -25,6 +31,10 @@ function loadPage() {
   devi = JSON.stringify(devi);
 
   $("#d").text("Devi: " + devi);
+
+  $("#e").text("S: " + window.innerWidth + " b " + window.innerHeight);
+
+  var screen
 
   // Hide or show the information
   $("#debug_toggle").click(function (e) {
@@ -57,6 +67,17 @@ function getDebugInfo() {
 
   data.cache = cache;
   data.cookies = Cookies.get();
+  data.device = {
+    platform: window.device.platform,
+    version: window.device.version,
+    cordova: window.device.cordova,
+    model: window.device.model,
+    manufacturer: window.device.manufacturer
+  };
+
+  data.connection = {
+    type: navigator.connection.type
+  };
 
   return data;
 }
